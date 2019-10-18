7f41e1c4781b9b0726a5bcadfbc8e3b34a7f8aca
Dean Attali <daattali@gmail.com>
2019-10-18 01:20:49 -0400

    some code improvements to validatedose and validatetime

--- validateTime.R
+++ validateTime.R
@@ -1,25 +1,39 @@
-# Validate time. Time can be entered either as a scaler, or as HHSS, or as HH:SS. 
+# Validate time. Time can be entered either as a scaler, or as HHSS, or as HH:SS.
 # Routine designed to accept pretty much anything and not return an error.
 validateTime <- function(x)
 {
+  if (length(x) > 1) {
+    stop("validateTime can only accept single items, not vectors.")
+  }
   # cat("x = ",x,"\n")
   # cat("is.null(x)"     , is.null(x),"\n")
   # cat("is.na(x)"       , is.na(x),"\n")
   # cat("is.numeric(x)"   , is.numeric(x),"\n")
   # cat("is.character(x)", is.character(x),"\n")
-  if (is.null(x) | is.na(x) | is.nan(x)) x <- ""
-  if (is.factor(x) | is.numeric(x)) x <- as.character(x)
-  # Remove everything except digits, colon, and decimal point
-  x <- gsub("[^[:digit:]:.]","",x)
-  if (x == "") return("")
-  # Remove second instance of a decimal point
-  x <- sapply(strsplit(x, "[.]"), function(y) 
-    if(length(y) >1) paste(y[1], paste(y[-1], collapse=""), sep=".") else y)
-  # Remove second instance of a colon
-  x <- sapply(strsplit(x, "[:]"), function(y) 
-    if(length(y) >1) paste(y[1], paste(y[-1], collapse=""), sep=":") else y)
-  # Return 0 if nothing is left
-  if (gsub("[:.]","",x) == "") return("0")
+  if (is.null(x) || is.na(x) || is.nan(x)) {
+    x <- ""
+  }
+  if (is.factor(x) || is.numeric(x)) {
+    x <- as.character(x)
+  }
+  # Remove everything except digits, colons, and decimal points
+  x <- gsub("[^0-9:.]","",x)
+  if (x == "") {
+    return("")
+  }
+  # Remove all decimal points and colons except the first ones
+  parts <- strsplit(x, "\\.")[[1]]
+  if (length(parts) > 1) {
+    x <- paste(parts[1], paste(parts[-1], collapse = ""), sep = ".")
+  }
+  parts <- strsplit(x, ":")[[1]]
+  if (length(parts) > 1) {
+    x <- paste(parts[1], paste(parts[-1], collapse = ""), sep = ":")
+  }
+  if (x == ".:" || x == ":.") {
+    return("") #TODO do you really want to return "0" here?
+  }
+
   # If there is a period, remove colon and return
   if (as.numeric(regexpr("[.]", x) > -1)) return (gsub("[^[:digit:].]","",x))
   colonPosition <- as.numeric(regexpr("[:]", x))
@@ -33,10 +47,11 @@ validateTime <- function(x)
     if (is.na(HH)) HH <- 0
     MM <- as.numeric(substr(x,colonPosition+1, 100))
     if (is.na(MM)) MM <- 0
-    # force 80 minutes into 1 hour and 20 minutes 
+    # force 80 minutes into 1 hour and 20 minutes
     HH <- HH + floor(MM/60)
-    MM <- MM %% 60 
+    MM <- MM %% 60
     x <- sprintf("%02d:%02d", HH, MM)
   }
   return(x)
+
 }
