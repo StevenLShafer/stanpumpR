5fd2837bc4d88d59c16a9d8a0d75806eabd72ecd
Thomas Shafer <thomasjshafer@gmail.com>
2020-03-08 19:05:02 -0700

    Add initial test for propofol.
    Change rounded expectation to use tolerance.

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,3 +1,5 @@
-expect_equal_rounded <- function (expected, actual, digits = 5) {
-  expect_equal(round(actual, digits), round(expected, digits))
+# https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/all.equal
+# Default tolerance is 1.5e-8
+expect_equal_rounded <- function (expected, actual, tolerance = 1.5e-6) {
+  expect_equal(actual, expected, tolerance = tolerance)
 }
--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -5,14 +5,14 @@ test_that("works for whole numbers", {
   expect_success(expect_equal_rounded(1, 1))
 })
 
-test_that("rounds with set digits", {
-  expect_success(expect_equal_rounded(1.111, 1.112, 2))
+test_that("can have a smaller tolerance", {
+  expect_success(expect_equal_rounded(1.111, 1.112, 1e-3))
 })
 
-test_that("rounds by default to 5 digits", {
+test_that("has a tolerance to 6 digits", {
   expect_success(expect_equal_rounded(1.1111111, 1.1111119))
 })
 
 test_that("fails if rounding does not produce the same values", {
-  expect_failure(expect_equal_rounded(1.11, 1.12, 2))
+  expect_failure(expect_equal_rounded(1.11, 1.12, 1e-3))
 })
