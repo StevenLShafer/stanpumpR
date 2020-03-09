1f48399f061847c2086cd78911e09f2c0cbe2abe
Thomas Shafer <thomasjshafer@gmail.com>
2020-03-08 20:31:33 -0700

    add test for methadone (#44)

---
+++ tests/testthat/test-drugs-methadone.R
@@ -0,0 +1,32 @@
+context("methadone")
+library(here)
+
+source(here('data', 'drugs', 'methadone.R'))
+
+test_that("returns the correct calculations", {
+  weight <- 70
+  height <- 171
+  age <- 50
+  sex <- "male"
+  actual <- methadone(weight, height, age, sex)
+
+  expected <- list(
+    PK = list(
+      default = list(
+        v1 = 12.6,
+        v2 = 85.68,
+        v3 = 1237.895,
+        cl1 = 0.101556,
+        cl2 = 5.1408,
+        cl3 = 1.4112
+      )
+    ),
+    tPeak = 11.3,
+    MEAC = 0.06,
+    typical = 0.072,
+    upperTypical = 0.048,
+    lowerTypical = 0.12,
+    reference = "Inturissi?"
+  )
+  expect_equal_rounded(actual, expected)
+})
