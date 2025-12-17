7799971b09dadbac1ae202b3a069fbafb38dd50d
Dean Attali <daattali@gmail.com>
2025-12-17 13:53:33 -0500

    revert probable mistakes in commit 45f39356a04e054e11479f072b5d0b83df8e5345 and add new drug to Collate field

--- data-raw/stanpumpR_tables.R
+++ data-raw/stanpumpR_tables.R
@@ -1,11 +1,4 @@
 library(devtools)
-library(openxlsx)
 drugDefaults_global <- read.csv("data-raw/drugDefaults_global.csv", stringsAsFactors = FALSE, na.strings = "")
 eventDefaults <- read.csv("data-raw/eventDefaults.csv", stringsAsFactors = FALSE)
 use_data(drugDefaults_global, eventDefaults, internal = TRUE, overwrite = TRUE)
-
-
-#drugDefaults_global <- read.xlsx("data/temp.xlsx")
-#write.xlsx(drugDefaults_global, file = "data/temp.xlsx")
-#write.csv(drugDefaults_global, file = "data/temp.csv")
-
