c315537e36b73e0fc68be88b195278882a17344b
Dean Attali <daattali@gmail.com>
2026-04-02 22:39:16 -0400

    make inputWithChoices have css as a dependency rather than in the css file

--- R/shiny-utils.R
+++ R/shiny-utils.R
@@ -36,6 +36,88 @@ addHotHooks <- function(hot, filterKeys = TRUE, sanitize = TRUE, ...) {
 #' the ID will be a concatenation of the provided input's ID together with "Choice".
 #' @param selected Which initial choice is selected
 inputWithChoices <- function(tag, choices, inputId = NULL, selected = NULL) {
+  css <- '
+.input-with-choices .main-choices-input {
+  left: -4px;
+  border-radius: 4px !important;
+}
+.input-with-choices .input-group-btn {
+  margin-top: 1px;
+}
+.input-with-choices .input-group-btn .form-group {
+  margin-bottom: 0;
+}
+.input-with-choices .input-group-btn .radio-inline {
+  padding: 0;
+  margin: 0;
+  margin-left: -5px;
+}
+.input-with-choices .input-group-btn .radio-inline:first-child {
+  margin-left: 0;
+}
+
+.input-with-choices .input-group-btn input[type="radio"] {
+  position: absolute;
+  width: 0;
+  height: 0;
+  opacity: 0;
+}
+
+.input-with-choices .input-group-btn input[type="radio"]:checked + span {
+  background-color: #2c3e50;
+  border-color: #2c3e50;
+  color: #ffffff;
+  box-shadow: inset 0 3px 5px rgba(0,0,0,0.125);
+}
+
+.input-with-choices .input-group-btn .radio-inline span {
+  display: inline-block;
+  padding: 6px 12px 5px 12px;
+  background: #ffffff;
+  border: 1px solid #ced4da;
+  color: #495057;
+  cursor: pointer;
+  transition: all 0.15s ease-in-out;
+}
+
+.input-with-choices .input-group-btn .radio-inline:first-child span {
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+.input-with-choices .input-group-btn .radio-inline:last-child span {
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+
+.input-with-choices .input-group-btn input[type="radio"]:focus + span {
+  border-color: #00264e;
+  box-shadow: 0 0 0 0.2rem rgba(0, 40, 75, 0.25);
+}
+
+.input-with-choices .input-group-btn .radio-inline span:hover {
+  background-color: #f4f8fb;
+  border-color: #b8c2cc;
+  color: #0056b3;
+}
+
+.input-with-choices .input-group-btn input[type="radio"]:checked + span:hover {
+  background-color: #0069d9;
+  border-color: #0062cc;
+}
+
+.input-with-choices .input-group-btn .radio-inline span:active {
+  background-color: #dae0e5;
+  box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+}
+  '
+
+  dep <- htmltools::htmlDependency(
+    name = "input-with-choices-styles",
+    version = "1.0.0",
+    src = c(href = ""),
+    head = sprintf("<style>%s</style>", css)
+  )
+
   input <- tag$children[[2]]  # WARNING in the future, if shiny changes internals, this can break
   input <- htmltools::tagAppendAttributes(input, class = "main-choices-input")
 
@@ -53,7 +135,7 @@ inputWithChoices <- function(tag, choices, inputId = NULL, selected = NULL) {
   )
   tag$children[[2]] <- input_group
 
-  tag
+  htmltools::attachDependencies(tag, dep)
 }
 
 #' Create an input that has its label on its left, and the input takes
