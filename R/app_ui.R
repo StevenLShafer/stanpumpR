3705f274670bb794047aeb2d48b4188f3b89a891
Dean Attali <daattali@gmail.com>
2026-04-22 23:18:48 -0400

    test pr2

--- R/app_ui.R
+++ R/app_ui.R
@@ -25,7 +25,7 @@ app_ui <- function() {
       ),
 
       bslib::nav_panel(
-        "Simulator",
+        "Simulator test2",
         icon = icon("chart-line"),
         bslib::layout_sidebar(
           sidebar = bslib::sidebar(
